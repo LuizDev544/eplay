e2149419a082116a45e5db1cd112fb16f07672ab
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-10-02 14:46:29 -0300

    Criando o Banner

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,30 @@
-import { HeaderBar } from './styles'
+import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+import logo from '../../assets/images/images/logo.svg'
+import carrinho from '../../assets/images/images/carrinho.svg'
 
-const Header = () => {
-  return <HeaderBar>Cabeçalho</HeaderBar>
-}
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categorias</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 - Produto(s)
+      <img src={carrinho} alt="Carrinho de compras" />
+    </LinkCart>
+  </HeaderBar>
+)
 
 export default Header
--- src/styles.ts
+++ src/styles.ts
@@ -13,15 +13,18 @@ export const GlobalCss = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
+    list-style: none;
   }
 
   body {
     background-color: ${cores.preta};
     color: ${cores.branca};
+    padding-top: 40px;
   }
-`
-export const Container = createGlobalStyle`
-  max-width: 1024px;
-  mwidth: 100%;
-  margin: 0 auto;
+
+  .container{
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
 `
