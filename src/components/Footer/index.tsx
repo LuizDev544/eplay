b2e53dbdf0851a8ad630158a425e009fe462c65c
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-10-13 14:34:18 -0300

    riando o footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => (
+  <div>
+    <div className="container">
+      <div>
+        <h4>Categorias</h4>
+      </div>
+      <div>
+        <h4>Acesso rápido</h4>
+      </div>
+      <p>2023 - &copy; E-eplay Todos os direitos reservados.</p>
+    </div>
+  </div>
+)
+
+export default Footer
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+import { cores } from '../../styles'
+
+export const Container = styled.footer`
+  background-color: ${cores.cinza};
+`
